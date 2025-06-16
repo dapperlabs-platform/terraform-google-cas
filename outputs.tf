d3c4aaa18d95f9783ed6741f0bdc0e02f930bd76
Alex Grieco <awgrieco@gmail.com>
2025-06-16 10:40:44 -0700

    Adding ca_certs to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "sa_email" {
   description = "The email of the service account with the permissions to request certificates from the subordinate CA pool."
   value       = module.sa-google-cas-issuer.email
 }
+
+output "ca_certs" {
+  description = "The CA certificate chain for the subordinate CA."
+  value       = google_privateca_certificate_authority.temporal-subordinate-ca.pem_ca_certificates
+}
